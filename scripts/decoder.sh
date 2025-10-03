a466c540ca4b615fae0ef8571121841f188e230e
Allen Gabrielle Cruiz <cruizallen2@gmail.com>
2025-10-04 04:14:02 +0800

    Update decoder.sh: change HEX_STRING to a placeholder for testing

--- scripts/decoder.sh
+++ scripts/decoder.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
+# Copyright (c) 2025 AlienWolfX
 
-HEX_STRING="81DFD45FFFFFFFFFF7F21FFFE2860C2BB"
+HEX_STRING="FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF" # Display All
 
 echo "Hex: $HEX_STRING"
 echo "Binary:"
 
 # Convert each hex digit to 4-bit binary
+# ZLT uses 4-bit segments for displaying pages in the dashboard
+# For example in Advanced Settings if the segments is 1001 it shows Flight Mode and Wan Settings
 for (( i=0; i<${#HEX_STRING}; i++ )); do
     hex_char="${HEX_STRING:$i:1}"
     bin_char=$(printf "%04d" "$(echo "ibase=16; obase=2; $hex_char" | bc)")
